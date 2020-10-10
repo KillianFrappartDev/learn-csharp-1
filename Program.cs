9b8fde4d6d5253183df6e860bf62d833e8e8d0f4
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-10-10 13:06:18 +0200

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_csharp_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
